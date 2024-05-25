eba84067e4f14483d37047632f177b57625d1aac
Utkarsh Patel <pateluttkarsh697@gmail.com>
2024-05-25 23:29:14 +0530

    onRamp, user and merchant added, bank webhook express app added

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useBalance } from "@repo/store/useBalance";
+import { Appbar } from "@repo/ui/Appbar";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function() {
-  const balance = useBalance();
+  const session = useSession()
   return <div>
-    hi there {balance}
+    <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user}/>
   </div>
 }
